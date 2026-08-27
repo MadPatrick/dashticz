c4fdfeb5df1888447b10cb92c7d1dc5be9e5b8b5
Patrick <81873830+MadPatrick@users.noreply.github.com>
2026-08-27 15:01:54 +0200

    Bump Dashticz asset version for hideimageonempty fix

--- js/loader.js
+++ js/loader.js
@@ -1,6 +1,6 @@
 /*global loadFiles */
 
-var _DASHTICZ_VERSION = 183;
+var _DASHTICZ_VERSION = 184;
 var head = document.getElementsByTagName('head')[0],
   script = document.createElement('script');
 
